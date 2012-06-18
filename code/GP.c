0c53a9d4318479420ef5fccbc23b6a49d72c98e1
Max Thrun <bear24rw@gmail.com>
2012-06-18 01:16:58 -0400

    [code] initial library port

---
+++ code/GP.c
@@ -0,0 +1,154 @@
+#include <msp430.h>
+#include <stdint.h>
+#include <GP.h>
+
+uint8_t gp_cur_spr = 0;    // current sprite, incremented by xsprite/xhide
+
+void GP_begin(void)
+{
+    delay(250);
+
+    // TODO:
+    // setup CS pin as output
+    // configure SPI
+    // set CS pin high
+
+    
+    GP_wr(J1_RESET, 1);           // HALT coprocessor
+    __wstart(RAM_SPR);            // Hide all sprites
+    for (uint16_t i = 0; i < 512; i++)
+        GP_xhide();
+    __end();
+    GP_fill(RAM_PIC, 0, 1024 * 10);  // Zero all character RAM
+    GP_fill(RAM_SPRPAL, 0, 2048);    // Sprite palletes black
+    GP_fill(RAM_SPRIMG, 0, 64 * 256);   // Clear all sprite data
+    GP_fill(VOICES, 0, 256);         // Silence
+    GP_fill(PALETTE16A, 0, 128);     // Black 16-, 4-palletes and COMM
+
+    GP_wr16(SCROLL_X, 0);
+    GP_wr16(SCROLL_Y, 0);
+    GP_wr(JK_MODE, 0);
+    GP_wr(SPR_DISABLE, 0);
+    GP_wr(SPR_PAGE, 0);
+    GP_wr(IOMODE, 0);
+    GP_wr16(BG_COLOR, 0);
+    GP_wr16(SAMPLE_L, 0);
+    GP_wr16(SAMPLE_R, 0);
+    GP_wr16(SCREENSHOT_Y, 0);
+    GP_wr(MODULATOR, 64);
+}
+
+
+void GP_sprite(uint16_t spr, uint16_t x, uint16_t y, uint8_t image, uint8_t palette, uint8_t rot, uint8_t jk)
+{
+  __wstart(RAM_SPR + (gp_cur_spr << 2));
+  spi_transfer(LOW_BYTE(x));
+  spi_transfer((palette << 4) | (rot << 1) | (HIGH_BYTE(x) & 1));
+  spi_transfer(LOW_BYTE(y));
+  spi_transfer((jk << 7) | (image << 1) | (HIGH_BYTE(y) & 1));
+  __end();
+}
+
+
+#include "font8x8.h"
+static uint8_t stretch[16] = {
+  0x00, 0x03, 0x0c, 0x0f,
+  0x30, 0x33, 0x3c, 0x3f,
+  0xc0, 0xc3, 0xcc, 0xcf,
+  0xf0, 0xf3, 0xfc, 0xff
+};
+
+void GP_ascii(void)
+{
+    uint16_t i;
+
+    for (i = 0; i < 768; i++) {
+        uint8_t b = font8x8[i];
+
+        uint8_t h = stretch[b >> 4];
+        uint8_t l = stretch[b & 0xf];
+        GP_wr(0x1000 + (16 * ' ') + (2 * i), h);
+        GP_wr(0x1000 + (16 * ' ') + (2 * i) + 1, l);
+    }
+    for (i = 0x20; i < 0x80; i++) {
+        GP_setpal(4 * i + 0, TRANSPARENT);
+        GP_setpal(4 * i + 3, RGB(255,255,255));
+    }
+    GP_fill(RAM_PIC, ' ', 4096);
+
+}
+
+void GP_putstr(uint16_t x, uint16_t y, const char *s)
+{
+    __wstart((y << 6) + x);
+    while (*s)
+        spi_transfer(*s++);
+    __end();
+}
+
+void GP_setpal(uint16_t pal, uint16_t rgb)
+{
+    GP_wr16(RAM_PAL + (pal << 1), rgb);
+}
+
+
+void GP_fill(uint16_t addr, uint8_t v, uint16_t count)
+{
+    __wstart(addr);
+    while (count--)
+        spi_transfer(v);
+    __end();
+}
+
+
+void GP_xhide(void)
+{
+    spi_transfer(LOW_BYTE(400));
+    spi_transfer(HIGH_BYTE(400));
+    spi_transfer(LOW_BYTE(400));
+    spi_transfer(HIGH_BYTE(400));
+    gp_cur_spr++;
+}
+
+void GP_wr(uint16_t addr, uint8_t v)
+{
+    __wstart(addr);
+    spi_transfer(v);
+    __end();
+}
+
+void GP_wr16(uint16_t addr, uint16_t v)
+{
+  __wstart(addr);
+  spi_transfer(LOW_BYTE(v));
+  spi_transfer(HIGH_BYTE(v));
+  __end();
+}
+
+void __wstart(uint16_t addr)
+{
+    __start(0x8000 | addr);
+}
+
+void __start(uint16_t addr)
+{
+    //TODO: CS pin LOW
+    spi_transfer(LOW_BYTE(addr));
+    spi_transfer(HIGH_BYTE(addr));
+}
+
+void __end(void)
+{
+    //TODO CS pin high
+}
+
+void spi_transfer(uint8_t byte)
+{
+    // TODO implement spi transfer
+}
+
+void delay(uint16_t ms)
+{
+    //TODO implement ms delay
+}
+
---
+++ code/GP.h
@@ -0,0 +1,71 @@
+#ifndef _GP_H_
+#define _GP_H_
+
+#include <stdint.h>
+
+#define LOW_BYTE(x) (x & 0xFF)
+#define HIGH_BYTE(x) (x >> 8)
+
+#define RGB(r,g,b) ((((r) >> 3) << 10) | (((g) >> 3) << 5) | ((b) >> 3))
+#define TRANSPARENT (1 << 15) // transparent for chars and sprites
+
+#define RAM_PIC     0x0000    // Screen Picture, 64 x 64 = 4096 bytes
+#define RAM_CHR     0x1000    // Screen Characters, 256 x 16 = 4096 bytes
+#define RAM_PAL     0x2000    // Screen Character Palette, 256 x 8 = 2048 bytes
+
+#define IDENT         0x2800
+#define REV           0x2801
+#define FRAME         0x2802
+#define VBLANK        0x2803
+#define SCROLL_X      0x2804
+#define SCROLL_Y      0x2806
+#define JK_MODE       0x2808
+#define J1_RESET      0x2809
+#define SPR_DISABLE   0x280a
+#define SPR_PAGE      0x280b
+#define IOMODE        0x280c
+
+#define BG_COLOR      0x280e
+#define SAMPLE_L      0x2810
+#define SAMPLE_R      0x2812
+
+#define MODULATOR     0x2814
+
+#define SCREENSHOT_Y  0x281e
+
+#define PALETTE16A 0x2840   // 16-color palette RAM A, 32 bytes
+#define PALETTE16B 0x2860   // 16-color palette RAM B, 32 bytes
+#define PALETTE4A  0x2880   // 4-color palette RAM A, 8 bytes
+#define PALETTE4B  0x2888   // 4-color palette RAM A, 8 bytes
+#define COMM       0x2890   // Communication buffer
+#define COLLISION  0x2900   // Collision detection RAM, 256 bytes
+#define VOICES     0x2a00   // Voice controls
+#define J1_CODE    0x2b00   // J1 coprocessor microcode RAM
+#define SCREENSHOT 0x2c00   // screenshot line RAM
+
+#define RAM_SPR     0x3000    // Sprite Control, 512 x 4 = 2048 bytes
+#define RAM_SPRPAL  0x3800    // Sprite Palettes, 4 x 256 = 2048 bytes
+#define RAM_SPRIMG  0x4000    // Sprite Image, 64 x 256 = 16384 bytes
+
+
+void GP_begin(void);
+
+void GP_sprite(uint16_t spr, uint16_t x, uint16_t y, uint8_t image, uint8_t palette, uint8_t rot, uint8_t jk);
+void GP_fill(uint16_t addr, uint8_t v, uint16_t count);
+void GP_xhide(void);
+void GP_ascii(void);
+void GP_setpal(uint16_t pal, uint16_t rgb);
+void GP_putstr(uint16_t x, uint16_t y, const char *s);
+
+void GP_wr(uint16_t addr, uint8_t v);
+void GP_wr16(uint16_t addr, uint16_t v);
+
+void __start(uint16_t addr);
+void __wstart(uint16_t addr);
+void __end(void);
+
+void spi_transfer(uint8_t byte);
+
+void delay(uint16_t ms);
+
+#endif
