5ac77dae63b5de189be28959c44bf7bc42a57e4b
Brittany Finch <bfinch09@ufl.edu>
2012-06-25 21:12:10 -0500

    added ccs project for palettes

---
+++ code/examples/1_basics/palettes/ccs/Debug/GP.pp
@@ -0,0 +1,17 @@
+# FIXED
+
+GP.obj: C:/cygwin/home/a0273894/gamepack/code/GP.c
+GP.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430.h
+GP.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h
+GP.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+GP.obj: C:/ti/ccsv5/tools/compiler/msp430_4.1.0/include/stdint.h
+GP.obj: C:/cygwin/home/a0273894/gamepack/code/GP.h
+GP.obj: C:/cygwin/home/a0273894/gamepack/code/font8x8.h
+
+C:/cygwin/home/a0273894/gamepack/code/GP.c: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.1.0/include/stdint.h: 
+C:/cygwin/home/a0273894/gamepack/code/GP.h: 
+C:/cygwin/home/a0273894/gamepack/code/font8x8.h: 
---
+++ code/examples/1_basics/palettes/ccs/Debug/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+C_SRCS += \
+C:/cygwin/home/a0273894/gamepack/code/GP.c \
+C:/cygwin/home/a0273894/gamepack/code/examples/1_basics/palettes/palettes.c 
+
+OBJS += \
+./GP.obj \
+./palettes.obj 
+
+C_DEPS += \
+./GP.pp \
+./palettes.pp 
+
+C_DEPS__QUOTED += \
+"GP.pp" \
+"palettes.pp" 
+
+OBJS__QUOTED += \
+"GP.obj" \
+"palettes.obj" 
+
+C_SRCS__QUOTED += \
+"C:/cygwin/home/a0273894/gamepack/code/GP.c" \
+"C:/cygwin/home/a0273894/gamepack/code/examples/1_basics/palettes/palettes.c" 
+
+
